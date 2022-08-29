f89c8562c430f4d5cb483c2453f68901b113cfbd
ahmad-hamdan98 <ahmadhamdan8592@gmail.com>
2022-08-29 12:53:27 +0300

    loops in js

---
+++ WEB FUNDAMENTALS 2021 AXSOS/javaScript/loops.js
@@ -0,0 +1,31 @@
+for(i=1;i<=20;i++){
+    if(i%2!=0){console.log(i,"odd number")}
+}
+console.log("first assaiment Done.");
+console.log("---------------------------------------------------------------------------------------------------------------")
+console.log("Secaned assaiment ...");
+for(i=100;i>=1;i--){
+    if(i%3==0){console.log(i,"divid number 3")}
+}
+console.log("Secaned assaiment Done.");
+console.log("---------------------------------------------------------------------------------------------------------------")
+console.log("3ed assaiment ...");
+for(i=4;i>-4;i-=1.5){
+    console.log(i);
+}
+console.log("---------------------------------------------------------------------------------------------------------------")
+
+console.log("4ed assaiment ...");
+var sigma =0;
+for(i=0;i<=100;i++){
+ sigma=sigma+i;
+}
+console.log(sigma,"sumation of 100");
+console.log("---------------------------------------------------------------------------------------------------------------")
+
+console.log("5ed assaiment ...");
+var prodect =1;
+for(i=1;i<=12;i++){
+ prodect=prodect*i;
+}
+console.log(prodect,"Factorial of 12");
