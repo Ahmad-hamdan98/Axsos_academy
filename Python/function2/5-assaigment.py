6c9aa53c17500c6f0c99abf8c9fd564f66a984f9
ahmad-hamdan98 <ahmadhamdan8592@gmail.com>
2022-09-12 16:34:48 +0300

    fynction-2

---
+++ Python/function2/5-assaigment.py
@@ -0,0 +1,46 @@
+
+# def countdown(input):
+#     arr=[]
+#     for input in range (input , 0,-1):
+#         arr.append(input)
+#     return arr
+
+# print(countdown(5))
+#_______________________first assaigment__________
+
+# def ret(a,b):
+#     print(a)
+#     return b
+
+# print(ret(1,2))
+#_______________________ assaigment 2__________
+# l=[1,2,3,4,5,6,7]
+# def sum(list):
+#     s=list[0]+list[len(list)-1]
+#     return s
+
+# print( sum(l))
+#_______________________ assaigment 3__________
+# arr1=[1,2,4,3,5]
+# def comp(arr):
+#     s=[]
+#     for i in range(0 ,len(arr) ,1):
+#         if arr[i]>2:
+#             s.append(arr[i])
+    
+    
+#     return s
+
+# print(comp(arr1))
+#_______________________ assaigment 4__________
+
+def rep(a,b):
+    arr=[]
+    for i in range (0, a,1):
+        arr.append(b)
+
+    return arr
+
+print(rep(6,7))
+#_______________________ assaigment 5__________
+
