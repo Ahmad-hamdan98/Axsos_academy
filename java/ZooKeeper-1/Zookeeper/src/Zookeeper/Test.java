ced27ffb4e58d7e2a7729ed1aed0b3b61c425591
ahmad-hamdan98 <ahmadhamdan8592@gmail.com>
2022-10-27 15:11:49 +0300

    Zoo1

---
+++ java/ZooKeeper-1/Zookeeper/src/Zookeeper/Test.java
@@ -0,0 +1,21 @@
+package Zookeeper;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Gorilla gor = new Gorilla();
+		
+		gor.throwSomething();
+		gor.throwSomething();
+		gor.throwSomething();
+		
+		gor.eatBananas();
+		gor.eatBananas();
+		
+		gor.climb();
+		
+
+	}
+
+}
