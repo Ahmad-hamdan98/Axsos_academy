234c45c16c144c9f2adbb7be9bb4d4f22cff6e1a
ahmad-hamdan98 <ahmadhamdan8592@gmail.com>
2022-11-03 20:25:05 +0200

    done

---
+++ java/MySgl/new/new/src/main/java/com/example/demo/services/BookService.java
@@ -0,0 +1,64 @@
+package com.example.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+//...
+import org.springframework.stereotype.Service;
+
+import com.example.demo.models.Book;
+import com.example.demo.repositories.BookRepository;
+
+
+
+@Service
+public class BookService {
+ // adding the book repository as a dependency
+	private final BookRepository bookRepository;
+ 
+ public BookService(BookRepository bookRepository) {
+     this.bookRepository = bookRepository;
+ }
+ // returns all the books
+ public List<Book> allBooks() {
+     return bookRepository.findAll();
+ }
+ // creates a book
+ public Book createBook(Book b) {
+     return bookRepository.save(b);
+ }
+ // retrieves a book
+ public Book findBook(Long id) {
+     Optional<Book> optionalBook = bookRepository.findById(id);
+     if(optionalBook.isPresent()) {
+         return optionalBook.get();
+     } else {
+         return null;
+     }
+ }
+public void deleteBook(Long id) {
+	// TODO Auto-generated method stub
+	  Optional<Book> optionalBook = bookRepository.findById(id);
+	     if(optionalBook.isPresent()) {
+	        bookRepository.delete(optionalBook.get());
+	     } 
+	
+	
+}
+public Book updateBook(Long id, String title, String desc, String lang, Integer numOfPages) {
+	// TODO Auto-generated method stub
+	 Optional<Book> optionalBook = bookRepository.findById(id);
+     if(optionalBook.isPresent()) {
+    	 optionalBook.get().setTitle(title);
+    	 optionalBook.get().setDescription(desc);
+    	 optionalBook.get().setLanguage(lang);
+    	 optionalBook.get().setNumberOfPages(numOfPages);
+    	 return bookRepository.save(optionalBook.get());
+     }else {
+         return null;
+     }
+	
+	
+}
+}
+
