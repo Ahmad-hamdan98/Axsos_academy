6e8506527b37879c692b57a19f446eed42e8430e
ahmad-hamdan98 <ahmadhamdan8592@gmail.com>
2022-12-06 20:51:02 +0200

    age+1

--- MARN/props/props/src/components/Propp.js
+++ MARN/props/props/src/components/Propp.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
 
 class Propp extends Component {
+  constructor (props){
+    super(props)
+    this.state={
+      add : this.props.age
+    }
+  }
+  
   render() {
     return (
       <div>
       <h1>  {this.props.firstname},{this.props.lastname}</h1>
-      <p>{this.props.age}</p>
+      {/* <p>{this.props.age}</p> */}
+      <p>{this.state.add}</p>
      <p> {this.props.hercolor}</p>
-     
+     <button onClick={ () =>  this.setState({ add:this.state.add+=1 }) } >age + 1</button>
       </div>
     )
   }
