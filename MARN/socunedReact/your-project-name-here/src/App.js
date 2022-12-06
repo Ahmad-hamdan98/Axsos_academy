a6a80014056e1f307b0a027d197cefe692f98998
ahmad-hamdan98 <ahmadhamdan8592@gmail.com>
2022-12-06 14:01:47 +0200

    seconedReact

---
+++ MARN/socunedReact/your-project-name-here/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Hello Dojo!</h1>
+    <h2>Things I need to Do</h2>
+    <ul>
+      <li>Learn React</li>
+      <li>Climp Mt. Everest</li>
+      <li>Run a Marathon</li>
+      <li>Feed the Dogs</li>
+    </ul>
+    </div>
+  );
+}
+
+export default App;
