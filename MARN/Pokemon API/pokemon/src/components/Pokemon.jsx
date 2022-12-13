4ed3a563639bbaa87479a91a23535048cb060488
ahmad-hamdan98 <ahmadhamdan8592@gmail.com>
2022-12-13 09:15:45 +0200

    put axios insted of fetch

--- MARN/Pokemon API/pokemon/src/components/Pokemon.jsx
+++ MARN/Pokemon API/pokemon/src/components/Pokemon.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react'
-
+import axios from 'axios';
 
 const Pokemon = () => {
     const [people, setPeople] = useState([]);
+    const [responseData, setResponseData] = useState([]);
     const [fet, setFet] = useState(false)
     useEffect(() => {
-        fetch('https://pokeapi.co/api/v2/pokemon/?limit=807')
-            .then(response => response.json())
-            .then(response => setPeople(response.results))
+        axios.get('https://pokeapi.co/api/v2/pokemon/?limit=809')
+            .then(response => { setResponseData(response.data.results) })
     }, []);
 
     const onclick = () => {
@@ -15,13 +15,14 @@ const Pokemon = () => {
     }
     return (
         <>
+        
+                {/* {responseData} */}
             <div>
-            <button onClick={onclick}>Fetch Pokemon</button>
-
+                <button onClick={onclick}>Fetch Pokemon</button>
                 {fet ?
-                people.map((person, index) => {
-                    return (<div key={index}>{person.name}</div>)
-                }):""}
+                    responseData.map((r, index) => {
+                        return (<div key={index}>{r.name}</div>)
+                    }) : ""}
             </div>
         </>
     );
